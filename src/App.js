f8bc63c92cc3ad1fd6f41d9ef8f13c3a92491979
Sinuhe Guerra <sinuhe.guerra@Sinuhes-MBP-2.users.condointernet.net>
2022-05-23 16:37:14 -0700

    Changing content.

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Site under construction.
         </p>
+        Being built by
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/sinuheg/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           Sinuhe Guerra 
         </a>
       </header>
     </div>
